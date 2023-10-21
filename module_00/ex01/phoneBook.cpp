45d1ec28c1b3791ffc436a5c942d52544d80fe72
mberrouk <mberrouk@student.1337.ma>
2023-10-21 16:10:38 +0100

    init

---
+++ module_00/ex01/phoneBook.cpp
@@ -0,0 +1,15 @@
+#include "phoneBook.h"
+
+int	main()
+{
+	char *s;
+
+	while (1)
+	{
+		std::cout << "<~> Enter command ~~~> ";
+		std::cin >> s;
+		std::cout << std::endl;
+		std::cout << s;
+	}
+	return (0);
+}
---
+++ module_00/ex01/phoneBook.h
@@ -0,0 +1,9 @@
+#ifndef PHONEBOOK_H
+#define PHONEBOOK_H
+
+#include <iostream>
+
+class PhoneBook {
+};
+
+#endif // !PHONEBOOK_H
